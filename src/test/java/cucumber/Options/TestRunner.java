7a8cff39e66ef9bde569e6bba348e8d54b2eac97
Serdar <serdar2225@gmail.com>
2020-10-11 08:03:30 -0400

    small name formattings

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinations"})
+@CucumberOptions(features="src/test/java/features",plugin ="json:target/jsonReports/cucumber-report.json",glue= {"stepDefinitions"})
 public class TestRunner {
 //tags= {"@DeletePlace"}  compile test verify
 }
